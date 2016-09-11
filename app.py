09b45018acc469c46e4bd1f89db853a2cf6556ee
Abhinav Jha <jhaab@us.ibm.com>
2016-09-10 23:49:04 -0400

    Cleaning

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index2.html')
+    return render_template('index.html')
+
+@app.route('/addTranscript', methods=['POST'])
+def addTranscript():
+   text = request.form['transcript']
+   with open('api/sample', 'w') as f:
+       f.write(text)
+   return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
